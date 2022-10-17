d51cc9a2c8adb5f9a69e468488f52480d656d4f3
Andre Pestana <andrepestana@gmail.com>
2022-10-16 19:22:16 -0700

    posting content

--- .vitepress/theme/components/MyLayout.vue
+++ .vitepress/theme/components/MyLayout.vue
@@ -4,14 +4,14 @@ import DefaultTheme from 'vitepress/theme'
 const { Layout } = DefaultTheme
 
 
-var disqus_config = function () {
-    this.page.url = window.location.href;  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = 'andre-pestana'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-};
-var d = document, s = d.createElement('script');
-s.src = 'https://andre-pestana.disqus.com/embed.js';
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
+// var disqus_config = function () {
+//     this.page.url = window.location.href;  // Replace PAGE_URL with your page's canonical URL variable
+//     this.page.identifier = 'andre-pestana'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+// };
+// var d = document, s = d.createElement('script');
+// s.src = 'https://andre-pestana.disqus.com/embed.js';
+// s.setAttribute('data-timestamp', +new Date());
+// (d.head || d.body).appendChild(s);
 </script>
 
 <template>
@@ -41,7 +41,7 @@ s.setAttribute('data-timestamp', +new Date());
             </div>
         </template>
         <template #doc-after>
-            <div id="disqus_thread"></div>
+
         </template>
         <!-- <template #aside-top></template>
         side-top
