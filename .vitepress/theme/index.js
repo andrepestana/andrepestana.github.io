1d585cc9919976f3e03b47cb6105d833a897b8a8
Andre Pestana <andrepestana@gmail.com>
2022-10-16 19:00:00 -0700

    posting content

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -11,13 +11,13 @@ export default {
     enhanceApp({ app, router, siteData }) {
         //
 
-        var disqus_config = function () {
-            this.page.url = window.location.href;  // Replace PAGE_URL with your page's canonical URL variable
-            this.page.identifier = 'andre-pestana'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-        };
-        var d = document, s = d.createElement('script');
-        s.src = 'https://andre-pestana.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
+        // var disqus_config = function () {
+        //     this.page.url = window.location.href;  // Replace PAGE_URL with your page's canonical URL variable
+        //     this.page.identifier = 'andre-pestana'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        // };
+        // var d = document, s = d.createElement('script');
+        // s.src = 'https://andre-pestana.disqus.com/embed.js';
+        // s.setAttribute('data-timestamp', +new Date());
+        // (d.head || d.body).appendChild(s);
     },
 };
